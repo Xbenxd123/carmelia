26411de7e6374c42a0b02de20590e1d7be922e95
LuisNiBos <ll643520@gmail.com>
2026-04-09 13:11:18 +0700

    feat: improvement view

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -1,32 +1,34 @@
-<?PHP
+<?php
     namespace App\Controllers;
+    require_once '../app/core/Controller.php';
+
+    use App\Core\Controller;
  
-    class StudentController
+    class StudentController extends Controller
     {
         public function index()
         {
-           require_once '../app/views/students/index.php';
+           $this->view('students.index');
         }
  
         public function create()
         {
-            require_once '../app/views/students/create.php';
-
+             $this->view('students.create');
         }
 
         public function show(string $id)
             {
-              require_once '../app/views/students/show.php';
+               $this->view('students.show');
             }
     
  
         public function edit(string $id)
         {
-            require_once '../app/views/students/edit.php';
+             $this->view('students.edit');
         }
 
 
-        }
+    }
  
 ?>
  
